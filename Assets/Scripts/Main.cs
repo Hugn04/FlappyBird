0d25d6580a780bde54ee3569bbb2763a8d2d9380
Nguyen Hung <hung2k4hytq@gmail.com>
2024-08-22 19:00:16 +0700

    Create Project

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    List<GameObject> pipeListDown;
+    List<GameObject> pipeListUp;
+    public GameObject prefabPipe;
+
+    void Start()
+    {
+        this.pipeListDown = new List<GameObject>();
+        this.Spawn();
+    }
+    void Update()
+    {
+
+
+
+
+
+        if (12- pipeListDown[pipeListDown.Count-1].transform.position.x > 5)
+        {
+             this.Spawn();
+        }
+    }
+    void Spawn()
+    {
+        Camera cam = Camera.main; 
+
+        float height = 2f * cam.orthographicSize;
+        Debug.Log(this.prefabPipe.transform.localScale.y / 2);
+        float width = height * cam.aspect;
+        float maxPosX = (width + this.prefabPipe.transform.localScale.x) / 2;
+        Quaternion rotate = transform.rotation;
+        Vector2 upPos = new Vector2(maxPosX, this.prefabPipe.transform.position.y);
+        GameObject pipeObjectUp = Instantiate(this.prefabPipe, upPos, rotate);
+        pipeObjectUp.transform.localScale = new Vector3(1.5f, Random.Range(5, 10), transform.localScale.z);
+        pipeObjectUp.SetActive(true);
+        this.pipeListDown.Add(pipeObjectUp);
+        Vector2  downPos = new Vector2(maxPosX, -height/2+ this.prefabPipe.transform.localScale.y/2);
+        GameObject pipeObjectDown = Instantiate(this.prefabPipe, downPos, rotate);
+        pipeObjectDown.transform.localScale = new Vector3(1.5f, Random.Range(5, 10), transform.localScale.z);
+        pipeObjectDown.SetActive(true);
+        this.pipeListDown.Add(pipeObjectDown);
+    }
+    // Update is called once per frame
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float jumpForce = 5f; // Lực nhảy
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Kiểm tra nếu nhấn phím nhảy (mặc định là Space)
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        // Áp dụng lực nhảy lên Rigidbody2D
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+   
+}
